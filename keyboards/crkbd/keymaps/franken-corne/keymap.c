22a6129d7c0c8e6205f8df4216d0256bd574eddb
Hans Kruse <hans.kruse@innvalor.nl>
2021-07-04 18:38:13 +0200

    fix left alt

--- keyboards/crkbd/keymaps/franken-corne/keymap.c
+++ keyboards/crkbd/keymaps/franken-corne/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                                   ,---------------------------------------------.
   KC_TILD,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                                      KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_HASH,
   //------+------+------+------+------+------|                                    |------+------+-------+------+-------+--------|
-  MEH_T(KC_TAB),  KC_A, LCTL_T(KC_S), RALT_T(KC_D), LGUI_T(KC_F), LSFT_T(KC_G),    RSFT_T(KC_H),  RGUI_T(KC_J),  RALT_T(KC_K),  RCTL_T(KC_L), KC_SCLN,KC_QUOT,
+  MEH_T(KC_TAB),  KC_A, LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), LSFT_T(KC_G),    RSFT_T(KC_H),  RGUI_T(KC_J),  RALT_T(KC_K),  RCTL_T(KC_L), KC_SCLN,KC_QUOT,
   //------+------+------+------+------+------|                                    |------+------+-------+------+-------+--------|
   LT(_MEDIA, KC_AT),  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                            KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH, LT(_FUNCTION_KEYS, KC_BSLS),
   //------+------+------+------+------+------+------|                             |------+------+------+-------+------+-------+--------|
